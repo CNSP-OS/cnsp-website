7051bdd07db05148a35dfa01202e9022ffc000a8
Akshi <AKSHITK40@gmail.com>
2020-02-03 19:38:28 +0530

    Migrated to EJS for better file handling
    Fixed a lot of hyperlinks
    Updated Readme
    Unified head, header, footer
    ETC

--- README.md
+++ README.md
@@ -3,14 +3,60 @@
 
 ### The previous closed source website has been replaced with an open source version and the code for the same is present above. Because the website development and migration was handled by one person only, you might find quite a lot of errors. It is expected they will be resolved very soon.
 
-## Development
+## Table of Contents
+1. [Issues, bugs, and requests](#issues-bugs-and-requests)
+2. [Prerequisites](#prerequisites) 
+3. [Before you build](#before-you-build)
+4. [Developing](#developing)
+5. [Redactions](#redactions)
 
-In order to contribute to the project, you must have the knowledge of HTML, CSS, and JS. Follow these steps:
 
-1) Fork the project & clone locally.
-2) Create an upstream remote and sync your local copy before you branch.
-3) Branch for each separate piece of work.
-4) Do the work, write good commit messages.
-5) Push to your origin repository.
-6) Create a new PR in GitHub.
-7) Respond to any code review feedback.
+## Issues, bugs, and requests
+
+We welcome contributions and feedback on our website! Please file a request in our  [issue tracker](https://github.com/flutter/website/issues/new)  and we'll take a look.
+
+For simple changes (such as to CSS and text), you probably don't need to build this site. Often you can make changes using the GitHub UI.
+
+If you want/need to build, read on.
+
+## Prerequisites
+Install the following tools if you don't have them already.
+
+-   **[Node.js](https://nodejs.org)**, a JavaScript runtime.
+-   **[npm](https://github.com/npm/cli)**, a JavaScript package manager.
+-   **[Express](https://expressjs.com/)**, Fast, unopinionated, minimalist web framework for [Node.js](https://nodejs.org/en/).
+-   **[Mongoose](https://expressjs.com/)**, elegant [mongodb](https://www.mongodb.com/) object modeling for [node.js](https://nodejs.org/en/).
+-   **[MongoDB](https://www.mongodb.com/community)**, MongoDB is a document database.
+-   **[EJS](https://www.mongodb.com/community)**, MongoDB is a document database.
+
+## Before you build
+
+### Clone this repo
+To **clone  [this repo](https://github.com/flutter/website)**, follow the instructions given in the GitHub help on [Cloning a repository](https://help.github.com/articles/cloning-a-repository), and _choose one_ of the following submodule-cloning techniques:
+
+### Run installation scripts
+**Open a bash terminal/command window**  and execute the following commands:
+
+1.  `cd  _<path-to-this-repo>_`  # change to  **root of this repo**
+2.  `npm i express mongoose ejs`  # initialize environment variables; install/use required Node & Ruby version
+
+### Delete lines
+Delete lines 5 to 41 in `server.js`
+
+### Running the server
+Run `npm start`
+
+## Developing
+
+In order to contribute to the project, you must have the knowledge of EJS, HTML, CSS, and JS. Follow these steps:
+
+1. Create a branch.
+2. Make your changes.
+3. Do the work, write good commit messages.
+4. Push to your origin repository.
+5. Create a new PR in GitHub.
+6. Respond to any code review feedback.
+
+## Redactions
+
+Certain parts of the server side code and the website have been redacted for security purposes. 
---
+++ server.js
@@ -0,0 +1,217 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:3332'); 
+var db=mongoose.connection; 
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+	console.log("connection succeeded"); 
+}) 
+app.post('REDACTED', function(req,res){ 
+// Redacted
+db.collection('REDACTED').insertOne(data,function(err, collection){ 
+		if (err) throw err; 
+		console.log("Record inserted Successfully"); 
+			
+	}); 
+		
+	return res.redirect('REDACTED'); 
+})
+app.post('REDACTED', function(req,res){ 
+// Redacted
+db.collection('REDACTED').insertOne(data,function(err, collection){ 
+		if (err) throw err; 
+		console.log("Record inserted Successfully"); 
+			
+	}); 
+		
+	return res.redirect('REDACTED'); 
+}) 
+app.post('REDACTED', function(req,res){ 
+// Redacted
+db.collection('REDACTED').insertOne(data,function(err, collection){ 
+		if (err) throw err; 
+		console.log("Record inserted Successfully"); 
+			
+	}); 
+		
+	return res.redirect('REDACTED'); 
+}) 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+app.use(express.static("assets"));
+// index page 
+app.get('/', function(req, res) {
+    res.locals.title = "CNSP - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/index');
+});
+app.get('/about/about_cnsp', function(req, res) {
+    res.locals.title = "About CNSP - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/about/about_cnsp');
+});
+app.get('/about/board', function(req, res) {
+    res.locals.title = "Board - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/about/board');
+});
+app.get('/about/brand_resources', function(req, res) {
+    res.locals.title = "Brand Resources - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/about/brand_resources');
+});
+app.get('/about/finance', function(req, res) {
+    res.locals.title = "Finance - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/about/finance');
+});
+app.get('/about/staff', function(req, res) {
+    res.locals.title = "Staff - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/about/staff');
+});
+app.get('/chapters', function(req, res) {
+    res.locals.title = "Chapters - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/chapters/index');
+});
+app.get('/chapters/new', function(req, res) {
+    res.locals.title = "New Chapter - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/chapters/new-chapter');
+});
+app.get('/chapters/asia', function(req, res) {
+    res.locals.title = "Asia - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/chapters/asia/index');
+});
+app.get('/chapters/asia/india', function(req, res) {
+    res.locals.title = "India - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/chapters/asia/india/index');
+});
+app.get('/chapters/asia/india/delhi', function(req, res) {
+    res.locals.title = "Delhi - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/chapters/asia/india/delhi/index');
+});
+app.get('/chapters/*', function(req, res) {
+    res.locals.title = "Not Found - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/chapters/chapter404');
+});
+app.get('/contact', function(req, res) {
+    res.locals.title = "Contact Us - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/contact/contact_us');
+});
+app.get('/faq', function(req, res) {
+    res.locals.title = "FAQ - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/contact/faq');
+});
+app.get('/support_desk', function(req, res) {
+    res.locals.title = "Support Desk - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/contact/support_desk');
+});
+app.get('/bylaws', function(req, res) {
+    res.locals.title = "ByLaws - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/legal/bylaws');
+});
+app.get('/privacypolicy', function(req, res) {
+    res.locals.title = "Privacy - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/legal/privacypolicy');
+});
+app.get('/projects', function(req, res) {
+    res.locals.title = "Projects - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/projects/index');
+});
+app.get('/projects/new', function(req, res) {
+    res.locals.title = "New Project - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/projects/new-project');
+});
+app.get('/projects/awareness', function(req, res) {
+    res.locals.title = "Awareness Projects - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/projects/awareness/index');
+});
+app.get('/projects/code', function(req, res) {
+    res.locals.title = "Development Projects - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/projects/code/index');
+});
+app.get('/projects/documentation', function(req, res) {
+    res.locals.title = "Documentation Projects - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/projects/documentation/index');
+});
+app.get('/projects/research', function(req, res) {
+    res.locals.title = "Research Projects - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/projects/research/index');
+});
+app.get('/donate', function(req, res) {
+    res.locals.title = "Donate - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/donate');
+});
+app.get('/membership', function(req, res) {
+    res.locals.title = "Membership - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/membership');
+});
+app.get('/search-results-page', function(req, res) {
+    res.locals.title = "Research Projects - Clear Net Security Project"; 
+    res.locals.description = null; 
+    res.locals.keywords = null;
+    res.render('pages/search-results-page');
+});
+
+app.use(function(req, res) {
+    res.locals.title = null; 
+    res.status(404).render('pages/status/404');
+  });
+  
+  // Handle 500
+  app.use(function(error, req, res, next) {
+    res.locals.title = null; 
+    res.status(500).render('pages/status/500');
+  });
+
+app.listen(80);
+console.log('BITCH');
